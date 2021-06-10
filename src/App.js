0434fab9b1d1d221b3fb906b4421b3b31a64f4d8
IanCo0k <73665952+IanCo0k@users.noreply.github.com>
2021-06-10 19:42:24 -0400

    beating mom

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className={'background'}>
+    <div className={'container'}>
+      <h1>Jeopardy Score Tracker</h1>
     </div>
+    <div className={'scores_container'}>
+      <div className={'ian'}>
+        <p>Ian Cook: $1,000</p>
+      </div>
+      <div className={'kendra'}>
+        <p>Kendra Cook: -$200</p>
+      </div>
+    </div>
+  </div>
   );
 }
 
