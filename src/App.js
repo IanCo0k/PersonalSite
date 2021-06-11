af6db4fad91f5ac8afb64cb6d7f66774b2e581ea
IanCo0k <73665952+IanCo0k@users.noreply.github.com>
2021-06-10 20:50:40 -0400

    jacob

--- src/App.js
+++ src/App.js
@@ -4,14 +4,12 @@ function App() {
   return (
   <div className={'background'}>
     <div className={'container'}>
-      <h1>Jeopardy Score Tracker</h1>
+      <h1>Hello Jacob Majoor</h1>
     </div>
     <div className={'scores_container'}>
       <div className={'ian'}>
-        <p>Ian Cook: $1,000</p>
-      </div>
-      <div className={'kendra'}>
-        <p>Kendra Cook: -$200</p>
+        <p>May I interest you in a game of 2k?</p>
+        <img src="https://www.insidehook.com/wp-content/uploads/2021/06/lebron-hate-watching-e1623167423144.jpg?fit=1156%2C800" alt="booty"/>
       </div>
     </div>
   </div>
